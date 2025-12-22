59bcc6772db7aadd4f8cacf479da2fb3f66effcb
octaveweb <karansproject955@gmail.com>
2025-12-22 21:49:48 +0530

    Navbar ✅

---
+++ dvsy/src/components/Main.jsx
@@ -0,0 +1,13 @@
+import Navbar from './Navbar'
+import Section1 from './Section1'
+
+function Main() {
+  return (
+    <main className='h-full bg-[#060503]'>
+        <Navbar />
+        <Section1 />
+    </main>
+  )
+}
+
+export default Main
---
+++ dvsy/src/components/Section1.jsx
@@ -0,0 +1,11 @@
+function Section1() {
+  return (
+    <div className="section1 px-5">
+        <div className="h-[100vh] bg-[url(../public/model.png)] bg-center bg-no-repeat bg-cover rounded-md">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Section1
